566966395aab043e81b0cadf39eb23d9becac71c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-23 17:32:18 +0000

    exported data to my script.js file to practice map / forEach etc

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import CATEGORIES from "./data.js";
+import { initialFacts } from "./data.js";
+
 const form = document.querySelector(".fact-form");
 const shareFact = document.querySelector(".share-fact");
 
@@ -9,3 +12,42 @@ shareFact.addEventListener("click", () => {
     shareFact.textContent = "Close";
   }
 });
+
+const allCategories = CATEGORIES.map((category) => category.name);
+
+console.log(allCategories);
+
+const facts = initialFacts.map((fact) => fact.text);
+
+console.log(facts);
+
+// const fact = ["lisbone is the capital", 2015, true];
+// console.log(fact[fact.length - 1]);
+// const [text, createdIn, x] = fact;
+// console.log(text, createdIn, x);
+
+// const newFact = [...fact, "society"];
+// console.log(newFact);
+
+// [2, 4, 6, 8].forEach((n) => console.log(n * 2));
+// const map = [2, 4, 6, 8].map((n) => n * 2);
+// console.log(map);
+
+// const factObject = {
+//   text: "lisbone is the capital of Portugal",
+//   category: "society",
+//   createdIn: 2015,
+//   isCorrect: true,
+//   createSummary: function () {
+//     return `The fact "${
+//       this.text
+//     }" is from the category ${this.category.toUpperCase()}`;
+//   },
+// };
+
+// console.log(factObject.text);
+// console.log(factObject["text"]);
+
+// const { category, isCorrect } = factObject;
+
+// console.log(factObject.createSummary());
