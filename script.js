6d9456b08f89e7904cbc5d1696dbb0649dfe82a7
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-23 11:20:37 +0000

    fixed a typo

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const shareFact = document.querySelector(".share-fact");
 shareFact.addEventListener("click", () => {
   form.classList.toggle("hidden");
   if (form.classList.contains("hidden")) {
-    shareFact.textContent = "Close";
-  } else {
     shareFact.textContent = "Share a Fact";
+  } else {
+    shareFact.textContent = "Close";
   }
 });
