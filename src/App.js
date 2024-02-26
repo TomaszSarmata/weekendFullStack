e157c6c2bcded2b9b67050b0541bf8abe65e83fe
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-26 11:08:16 +0000

    Created another component in App.js and included it as an JSX in the App component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,50 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img
-          src="logo.png"
-          alt="Today I learned logo"
-          height="68px"
-          width="68px"
-        />
-        <h1>Today I learned</h1>
-      </div>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            alt="Today I learned logo"
+            height="68px"
+            width="68px"
+          />
+          <h1>Today I learned</h1>
+        </div>
 
-      <button className="btn btn-large share-fact">Share A Fact</button>
-    </header>
+        <button className="btn btn-large share-fact">Share A Fact</button>
+      </header>
+      <CategoryFilter />
+    </>
+  );
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li class="category">
+          <button class="btn btn-all-categories">All</button>
+        </li>
+        <li class="category">
+          <button
+            class="btn btn-category"
+            styleName="background-color: #3b82f6"
+          >
+            Technology
+          </button>
+        </li>
+        <li class="category">
+          <button
+            class="btn btn-category"
+            styleName="background-color: #16a34a"
+          >
+            Science
+          </button>
+        </li>
+      </ul>
+    </aside>
   );
 }
 
