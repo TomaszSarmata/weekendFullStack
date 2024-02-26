47fa9bb39802b3eacd413fee424c84da2baac43a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-02-26 10:55:56 +0000

    Implemented header in the main App folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./style.css";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img
+          src="logo.png"
+          alt="Today I learned logo"
+          height="68px"
+          width="68px"
+        />
+        <h1>Today I learned</h1>
+      </div>
+
+      <button className="btn btn-large share-fact">Share A Fact</button>
+    </header>
+  );
 }
 
 export default App;
